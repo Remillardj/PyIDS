0c9660b5e31525963c73970a2fe45a9d447bec91
Remillardj <jaryd.remillard@gmail.com>
2018-01-28 22:37:43 -0800

    adding display all network interfaces

--- modules/info.py
+++ modules/info.py
@@ -1,8 +1,14 @@
+import socket
+import fcntl
+import struct
+import array
 
-class pyIDS_info(object):
-
+class idsInfo(object):
+    def all_interfaces():
+        maxInt = 128
+        byte = maxInt *32
+        s = socket.socket(socket.AF_INET, socket.SOCKDGRAM)
+        
     def get_mac_gateway(ip_address):
         response, unanswered = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip_address), \
         timeout = 2, retry = 2)
-
-    
